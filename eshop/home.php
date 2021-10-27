0335b4dbb0069ebfcb2f0b7362de6dcaad111e42
Melvin <ngwingchun5041@e.newera.edu.my>
2021-10-27 12:47:32 +0800

    Web Application Tutorial

---
+++ eshop/home.php
@@ -0,0 +1,48 @@
+<!--ID : 2030346 -->
+<!--Name : NG WING Chun -->
+<!--Topic : Eshop Home-->
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Home</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+
+<body class="bg-image" style="background-image: url('img/wallpaper.jpg');">
+    <div class="container-fuild bg-dark">
+        <div class="container">
+
+            <nav class="navbar-expand-lg py-2">
+
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-secondary" href="product_create.php">Create Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-secondary" href="customer_create.php">Create Customer</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link text-secondary" href="contact_us.php">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="text-center p-5">
+        <h1 class="text-white fw-bold">Welcome</h1>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>
